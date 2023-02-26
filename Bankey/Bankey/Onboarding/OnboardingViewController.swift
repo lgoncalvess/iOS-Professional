717bd82b6e6478f707cd074140d785123d1af6ae
Leonardo Gonçalves <leonardo@MacBook-Pro-de-Leonardo.local>
2023-02-26 16:01:33 -0300

    add first onboarding screen

---
+++ Bankey/Bankey/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,52 @@
+//
+//  OnboardingViewController.swift
+//  Bankey
+//
+//  Created by Leonardo Gonçalves on 26/02/23.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    let stackView = UIStackView()
+    let imageView = UIImageView()
+    let label = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension OnboardingViewController {
+    func style() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "delorean")
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.adjustsFontForContentSizeCategory = true
+        label.numberOfLines = 0
+        label.text = "delorean delorean delorean delorean delorean delorean delorean delorean."
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+    }
+    
+    func layout() {
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(label)
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
+        ])
+    }
+}

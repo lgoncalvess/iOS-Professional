2fbef1749d58ac7531ca51113502cb7711592166
Leonardo Gonçalves <leonardo@MacBook-Pro-de-Leonardo.local>
2023-03-11 12:06:21 -0300

    feat: add dispatch group

--- Bankey/Bankey/AccountSummary/AccountSummaryViewController.swift
+++ Bankey/Bankey/AccountSummary/AccountSummaryViewController.swift
@@ -29,20 +29,15 @@ class AccountSummaryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
-        setupNavigationBar()
-    }
-    
-    func setupNavigationBar() {
-        navigationItem.rightBarButtonItem = logoutBarButtonItem
     }
 }
 
 extension AccountSummaryViewController {
     private func setup() {
+        setupNavigationBar()
         setupTableView()
         setupTableHeaderView()
-//        fetchAccounts()
-        fetchDataAndLoadViews()
+        fetchData()
     }
     
     private func setupTableView() {
@@ -71,6 +66,10 @@ extension AccountSummaryViewController {
         size.width = UIScreen.main.bounds.width
         headerView.frame.size = size
         tableView.tableHeaderView = headerView
+    }
+    
+    func setupNavigationBar() {
+        navigationItem.rightBarButtonItem = logoutBarButtonItem
     }
 }
 
@@ -101,8 +100,10 @@ extension AccountSummaryViewController {
 }
 
 extension AccountSummaryViewController {
-    private func fetchDataAndLoadViews() {
+    private func fetchData() {
+        let group = DispatchGroup()
         
+        group.enter()
         fetchProfile(forUserId: "1") { result in
             switch result {
             case .success(let profile):
@@ -112,8 +113,10 @@ extension AccountSummaryViewController {
             case .failure(let error):
                 print(error.localizedDescription)
             }
+            group.leave()
         }
 
+        group.enter()
         fetchAccounts(forUserId: "1") { result in
             switch result {
             case .success(let accounts):
@@ -123,6 +126,10 @@ extension AccountSummaryViewController {
             case .failure(let error):
                 print(error.localizedDescription)
             }
+            group.leave()
+        }
+        group.notify(queue: .main){
+            self.tableView.reloadData()
         }
     }
     
